546fa215a9011a7a1782aed3d456b0adc59848cc
Markus Mayer <mmayer@mayeranalytics.com>
2017-09-05 08:12:31 +0200

    initial commit

---
+++ Tables.py
@@ -0,0 +1,12 @@
+"""Various tables collected from the datasheet."""
+
+# map gnssId to GNSS string. §31.6
+GNSS_Identifiers = [
+    'GPS',
+    'SBAS',
+    'Galileo',
+    'BeiDou',
+    'IMES',
+    'QZSS',
+    'GLONASS',
+    ]
