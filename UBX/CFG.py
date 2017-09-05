01c9cb01ab0ed18b9baca00529e5c6e21056ba43
Markus Mayer <mmayer@mayeranalytics.com>
2017-09-05 08:13:11 +0200

    update

--- UBX/CFG.py
+++ UBX/CFG.py
@@ -2,7 +2,7 @@
 
 from UBXMessage import UBXMessage, initMessageClass, addGet
 import struct
-from Types import U1, U2, X4
+from Types import U1, U2, U4, X4, U
 
 
 @initMessageClass
@@ -48,3 +48,23 @@ class CFG:
                 maxTrkCh = U1(3)
                 reserved = U1(4)
                 flags = X4(5)
+
+    @addGet
+    class PM2:
+        """§31.11.20.2 Extended Power Management configuration."""
+
+        _id = 0x3B
+
+        class Fields:
+            version = U1(1)  # Message version (0x02 for this version)
+            reserved1 = U1(2)  # Reserved
+            maxStartupStartupDur = U1(3)  # Maximum time to spend in Acquisition state. If 0: bound disabled (see maxStartupStateDur). (not supported in protocol versions less than 17), (not supported in protocol versions 23 to 23.01)
+            reserved2 = U1(4)  # Reserved
+            flags = X4(5)  # PSM configuration flags (see graphic below)
+            updatePeriod = U4(6) 	# ms  Position update period. If set to 0, the receiver will never retry a fix and it will wait for external events
+            searchPeriod = U4(7) 	# ms  Acquisition retry period if previously failed. If set to 0, the receiver will never retry a startup (not supported in protocol versions 23 to 23.01)
+            gridOffset = U4(8) 	# ms  Grid offset relative to GPS start of week (not supported in protocol versions 23 to 23.01)
+            onTime = U2(9)  # s  Time to stay in Tracking state (not supported in protocol versions 23 to 23.01)
+            minAcqTime = U2(10)  # s  minimal search time
+            reserved3 = U(11, 20) 	# Reserved
+            extintInactivityMs = U4(12)  # ms  inactivity time out on EXTINT pint if enabled
