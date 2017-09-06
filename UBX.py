dc8e47b294eef99697226e0434c192e7f142c155
Markus Mayer <mmayer@mayeranalytics.com>
2017-09-06 10:03:23 +0200

    start adding cmd line args

--- UBX.py
+++ UBX.py
@@ -7,13 +7,44 @@ import serial
 from time import sleep
 from UBXManager import UBXManager
 import UBX
+import argparse
+
+
+class Manager(UBXManager):
+    def __init__(self, debug=debug):
+        UBXManager.__init__(debug)
+    def onUBX(self, obj):
+        print(obj)
+    def onUBXError(self, msgClass, msgId, errMsg):
+        print(msgClass, msgId, errMsg)
 
 if __name__ == '__main__':
 
-    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=None)
-    debug = os.environ.get("DEBUG") is not None
+    parser = argparse.ArgumentParser(
+        description='Send UBX commands to u-blox M8 device.'
+        )
+    parser.add_argument(
+        '--VER', dest='VER', action='store_true',
+        help='Get the version string'
+        )
+    parser.add_argument(
+        '--RXM', dest='RXM', action='store',
+        help='Set the power mode (0=cont, 1=save)'
+        )
+    parser.add_argument(
+        '-d', '--debug', dest='debug', action='store_true',
+        help='Turn on debug mode'
+        )
+    args = parser.parse_args()
 
-    manager = UBXManager(ser, debug=debug)
+    todo = []
+    if args.VER: todo.append(("VER", None))
+    if args.RXM is not None: todo.append(("RXM", args.RXM))
+
+    ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=None)
+    debug = (os.environ.get("DEBUG") is not None) or args.debug
+
+    manager = Manager(ser, debug=debug)
     manager.start()
     sys.stderr.write("started UBXManager\n")
 
