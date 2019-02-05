91af2ee2d703045d27083d9c3d07e821447a178f
MM <mmayer@mayeranalytics.com>
2019-02-05 10:27:35 +0100

    break the circular dependency UBXMessage->UBX->UBXManager->UBXMessage

--- UBXMessage.py
+++ UBXMessage.py
@@ -6,7 +6,6 @@ import inspect
 from enum import Enum
 import sys
 from itertools import accumulate
-import UBX
 
 
 class MessageClass(Enum):
@@ -289,6 +288,7 @@ def addGet(cls):
     class Get(UBXMessage):
         def __init__(self):
             # this only works because class and module have the same name!
+            import UBX
             _class = eval(cls.__module__)._class
             UBXMessage.__init__(self, _class, cls._id, b'')
     setattr(cls, "Get", Get)
